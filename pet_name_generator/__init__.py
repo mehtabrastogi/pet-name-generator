bca4423214af74b715309907d1e04868981e842a
Mehtab Rastogi <mehtabrastogi@gmail.com>
2026-06-16 15:08:18 +0530

    feat: Add package initialization

---
+++ pet_name_generator/__init__.py
@@ -0,0 +1,11 @@
+"""
+Pet Name Generator - Generate unique and creative names for your pets.
+"""
+
+from .generator import PetNameGenerator
+
+__version__ = "1.0.0"
+__author__ = "Mehtab Rastogi"
+__license__ = "MIT"
+
+__all__ = ["PetNameGenerator"]
